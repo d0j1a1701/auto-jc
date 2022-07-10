d280ba355960466185b665fb5cfe1076e2cd4882
d0j1a_1701 <48874489+dajia-1701@users.noreply.github.com>
2022-07-10 12:51:03 +0800

    Create jc.js

---
+++ jc.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         自动机惨系统
+// @namespace    https://www.d0j1a1701.cc
+// @version      0.1
+// @description  IAKIOI!
+// @author       d0j1a_1701
+// @match        https://www.luogu.com.cn/
+// @icon         https://www.google.com/s2/favicons?domain=luogu.com.cn
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.getElementById('feed-content').value = 'I AK IOI';
+    document.getElementById('feed-submit').click();
+    // Your code here...
+})();
