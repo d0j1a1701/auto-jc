7522448a49d374b098a531c821342301f3a51d32
d0j1a_1701 <48874489+dajia-1701@users.noreply.github.com>
2022-07-10 14:14:56 +0800

    Update jc.user.js

--- jc.user.js
+++ jc.user.js
@@ -9,9 +9,11 @@
 // @grant        none
 // ==/UserScript==
 
+let words = ['I AK IOI','我在此郑重宣布 I AK IOI','洛谷将会臭名昭著!','我将在三天之内撕碎洛谷脆弱的防火墙!','我AK5次IOI!']
+
 (function() {
     'use strict';
-    document.getElementById('feed-content').value = 'I AK IOI';
+    document.getElementById('feed-content').value = words[Math.floor(Math.random()*words.length)];
     document.getElementById('feed-submit').click();
     // Your code here...
 })();
