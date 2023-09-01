bc7f69462950a7568558a8073931fa3ac50477ef
Lewis Lucas <48874489+d0j1a1701@users.noreply.github.com>
2023-09-01 20:56:04 +0800

    Add files via upload

---
+++ cookies-old.py
@@ -0,0 +1,51 @@
+import os
+import json
+import base64
+import sqlite3
+from win32crypt import CryptUnprotectData
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+
+def get_string(local_state):
+    with open(local_state, 'r', encoding='utf-8') as f:
+        s = json.load(f)['os_crypt']['encrypted_key']
+    return s
+
+
+def pull_the_key(base64_encrypted_key):
+    encrypted_key_with_header = base64.b64decode(base64_encrypted_key)
+    encrypted_key = encrypted_key_with_header[5:]
+    key = CryptUnprotectData(encrypted_key, None, None, None, 0)[1]
+    return key
+
+
+def decrypt_string(key, data):
+    nonce, cipherbytes = data[3:15], data[15:]
+    aesgcm = AESGCM(key)
+    plainbytes = aesgcm.decrypt(nonce, cipherbytes, None)
+    plaintext = plainbytes.decode('utf-8')
+    return plaintext
+
+
+def get_cookie_from_chrome(host='.luogu.com.cn'):
+    local_state = r'C:\Program Files (x86)\Google Chrome\Chrome\Data\Local State'
+    cookie_path = r'C:\Program Files (x86)\Google Chrome\Chrome\Data\Cookies'
+    
+    sql = "select host_key,name,encrypted_value from cookies where host_key='%s'" % host
+
+    with sqlite3.connect(cookie_path) as conn:
+        cu = conn.cursor()
+        res = cu.execute(sql).fetchall()
+        cu.close()
+        cookies = {}
+        key = pull_the_key(get_string(local_state))
+        for host_key, name, encrypted_value in res:
+            if encrypted_value[0:3] == b'v10':
+                cookies[name] = decrypt_string(key, encrypted_value)
+            else:
+                cookies[name] = CryptUnprotectData(encrypted_value)[1].decode()
+
+        # print(cookies)
+        return cookies
+
+print(get_cookie_from_chrome('.luogu.com.cn'))
